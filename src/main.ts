42dbb0b3451a73d05a97a720f278619edc8b6d50
H. Cortez <100394133+HenryCortez@users.noreply.github.com>
2025-05-06 10:18:57 -0500

    Update main.ts
    Change settings to use in render

--- src/main.ts
+++ src/main.ts
@@ -38,8 +38,8 @@ async function bootstrap() {
       'Complete API documentation for the GITT application. This API is designed to provide a comprehensive set of endpoints for managing and interacting with the GITT application.',
     )
     .setVersion('1.0')
-    .addServer(`http://localhost:${port}`, 'Servidor local')
-    // .addServer('https://api.produccion.com', 'Servidor de producción')
+    //.addServer(`http://localhost:${port}`, 'Servidor local')
+    .addServer('https://gitt-api-3tw6.onrender.com', 'Servidor de producción')
     .addBearerAuth({
       type: 'http',
       scheme: 'bearer',
