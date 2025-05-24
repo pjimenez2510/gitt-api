9a439422bfc212b2671568d30d1294c5c357b042
pjimenez2510 <pjimenez2510@gmail.com>
2025-05-24 00:47:12 -0500

    feat(item-colors): add filtering capabilities for item colors with new DTOs and utility functions

--- src/core/item-colors/item-colors.service.ts
+++ src/core/item-colors/item-colors.service.ts
@@ -1,43 +1,45 @@
 import { HttpStatus, Injectable, NotFoundException } from '@nestjs/common'
 import { plainToInstance } from 'class-transformer'
-import { and, count, desc, eq } from 'drizzle-orm'
-import { BaseParamsDto } from 'src/common/dtos/base-params.dto'
-import { excludeColumns } from 'src/common/utils/drizzle-helpers'
+import { and, count, desc, eq, not } from 'drizzle-orm'
 import { DatabaseService } from 'src/global/database/database.service'
 import { ItemColorResDto } from './dto/res/item-color-res.dto'
 import { CreateItemColorDto } from './dto/req/create-item-color.dto'
 import { DisplayableException } from 'src/common/exceptions/displayable.exception'
 import { UpdateItemColorDto } from './dto/req/update-item-color.dto'
-import { itemColor } from 'drizzle/schema/tables/inventory/item'
+import { itemColor } from 'drizzle/schema/tables/inventory/item/itemColor'
+import { FilterItemColorDto } from './dto/req/item-color-filter.dto'
+import { itemColorColumnsAndWith } from './const/item-color-columns-and-with'
+import {
+  buildItemColorFilterConditions,
+  buildItemColorWhereClause,
+} from './utils/item-color-filter-builder'
 
 @Injectable()
 export class ItemColorsService {
   constructor(private readonly dbService: DatabaseService) {}
 
-  private readonly itemColorsWithoutDates = excludeColumns(
-    itemColor,
-    'registrationDate',
-    'updateDate',
-  )
+  async findAll(filterDto: FilterItemColorDto) {
+    const conditions = buildItemColorFilterConditions(filterDto)
+    const whereClause = buildItemColorWhereClause(conditions)
 
-  async findByItemId({ limit, page }: BaseParamsDto, itemId: number) {
-    const offset = (page - 1) * limit
+    const offset = (filterDto.page - 1) * filterDto.limit
 
-    const query = this.dbService.db
-      .select(this.itemColorsWithoutDates)
-      .from(itemColor)
-      .where(eq(itemColor.itemId, itemId))
-      .orderBy(desc(itemColor.id))
-      .limit(limit)
-      .offset(offset)
+    const query = this.dbService.db.query.itemColor.findMany({
+      where: whereClause,
+      with: itemColorColumnsAndWith.with,
+      columns: itemColorColumnsAndWith.columns,
+      orderBy: [desc(itemColor.id)],
+      limit: filterDto.allRecords ? undefined : filterDto.limit,
+      offset: filterDto.allRecords ? undefined : offset,
+    })
 
     const totalQuery = this.dbService.db
       .select({ count: count() })
       .from(itemColor)
-      .where(eq(itemColor.itemId, itemId))
+      .where(whereClause)
 
     const [records, totalResult] = await Promise.all([
-      query.execute(),
+      query,
       totalQuery.execute(),
     ])
 
@@ -46,41 +48,67 @@ export class ItemColorsService {
     return {
       records: plainToInstance(ItemColorResDto, records),
       total,
-      limit,
-      page,
-      pages: Math.ceil(total / limit),
+      limit: filterDto.allRecords ? total : filterDto.limit,
+      page: filterDto.allRecords ? 1 : filterDto.page,
+      pages: filterDto.allRecords ? 1 : Math.ceil(total / filterDto.limit),
     }
+  }
+
+  async existById(id: number) {
+    const [record] = await this.dbService.db
+      .select({ id: itemColor.id })
+      .from(itemColor)
+      .where(and(eq(itemColor.id, id), eq(itemColor.active, true)))
+      .limit(1)
+      .execute()
+
+    return !!record
   }
 
   async findOne(id: number) {
-    const [record] = await this.dbService.db
-      .select(this.itemColorsWithoutDates)
-      .from(itemColor)
-      .where(eq(itemColor.id, id))
-      .limit(1)
-      .execute()
+    const record = await this.dbService.db.query.itemColor.findFirst({
+      where: and(eq(itemColor.id, id), eq(itemColor.active, true)),
+      columns: itemColorColumnsAndWith.columns,
+      with: itemColorColumnsAndWith.with,
+    })
+
     if (!record) {
-      throw new NotFoundException(`ítem/Color con id ${id} no encontrado`)
+      throw new NotFoundException(`Ítem/Color con id ${id} no encontrado`)
     }
+
     return plainToInstance(ItemColorResDto, record)
   }
 
-  async create(dto: CreateItemColorDto) {
-    const [alreadyExistItemColor] = await this.dbService.db
-      .select(this.itemColorsWithoutDates)
-      .from(itemColor)
-      .where(
+  async existsItemAndColorCombination(
+    itemId: number,
+    colorId: number,
+    excludeId?: number,
+  ) {
+    const record = await this.dbService.db.query.itemColor.findFirst({
+      where: and(
         and(
-          eq(itemColor.itemId, dto.itemId),
-          eq(itemColor.colorId, dto.colorId),
+          eq(itemColor.itemId, itemId),
+          eq(itemColor.colorId, colorId),
+          eq(itemColor.active, true),
+          excludeId ? not(eq(itemColor.id, excludeId)) : undefined,
         ),
-      )
-      .limit(1)
-      .execute()
+      ),
+      columns: itemColorColumnsAndWith.columns,
+      with: itemColorColumnsAndWith.with,
+    })
+
+    return !!record
+  }
+
+  async create(dto: CreateItemColorDto) {
+    const alreadyExistItemColor = await this.existsItemAndColorCombination(
+      dto.itemId,
+      dto.colorId,
+    )
 
     if (alreadyExistItemColor) {
       throw new DisplayableException(
-        'Ya existe una combiancion de item y ese color',
+        'Ya existe una combinación de ítem y ese color',
         HttpStatus.CONFLICT,
       )
     }
@@ -88,41 +116,60 @@ export class ItemColorsService {
     const [newItemColor] = await this.dbService.db
       .insert(itemColor)
       .values(dto)
-      .returning(this.itemColorsWithoutDates)
+      .returning()
       .execute()
+
     return plainToInstance(ItemColorResDto, newItemColor)
   }
 
   async update(id: number, dto: UpdateItemColorDto) {
-    await this.findOne(id)
+    const exists = await this.existById(id)
+
+    if (!exists) {
+      throw new NotFoundException(`Ítem/Color con id ${id} no encontrado`)
+    }
+
+    if (dto.colorId) {
+      const currentRecord = await this.findOne(id)
+
+      const alreadyExistItemColor = await this.existsItemAndColorCombination(
+        currentRecord.itemId,
+        dto.colorId,
+        id,
+      )
+
+      if (alreadyExistItemColor) {
+        throw new DisplayableException(
+          'Ya existe una combinación de ítem y ese color',
+          HttpStatus.CONFLICT,
+        )
+      }
+    }
 
     const [updatedItemColor] = await this.dbService.db
       .update(itemColor)
       .set(dto)
       .where(eq(itemColor.id, id))
-      .returning(this.itemColorsWithoutDates)
+      .returning()
       .execute()
 
     return plainToInstance(ItemColorResDto, updatedItemColor)
   }
 
-  //sin borrado logico
   async remove(id: number) {
-    await this.findOne(id)
+    const exists = await this.existById(id)
 
-    const [deletedItemColor] = await this.dbService.db
+    if (!exists) {
+      throw new NotFoundException(`Ítem/Color con id ${id} no encontrado`)
+    }
+
+    const [updatedItemColor] = await this.dbService.db
       .update(itemColor)
-      .set({ active: false })
+      .set({ active: false, updateDate: new Date() })
       .where(eq(itemColor.id, id))
-      .returning(this.itemColorsWithoutDates)
+      .returning({ active: itemColor.active })
       .execute()
 
-    if (!deletedItemColor) {
-      throw new DisplayableException(
-        `Error al eliminar el item-color con id ${id}`,
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      )
-    }
-    return plainToInstance(ItemColorResDto, deletedItemColor)
+    return updatedItemColor.active === false
   }
 }
