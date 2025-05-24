606726443b7709ffce33e36d08074e52ce6029e9
pjimenez2510 <pjimenez2510@gmail.com>
2025-05-24 00:54:50 -0500

    feat(item-materials): implement filtering capabilities for item materials with new DTOs and utility functions

--- src/core/item-materials/dto/res/item-material-res.dto.ts
+++ src/core/item-materials/dto/res/item-material-res.dto.ts
@@ -1,38 +1,33 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { Exclude, Expose } from 'class-transformer'
+import { MaterialResDto } from 'src/core/materials/dto/res/material-res.dto'
 
 export class ItemMaterialResDto {
   @ApiProperty({
     description: 'id única de la combinacion de material e item',
     example: 1,
   })
-  @Expose()
   id: number
 
   @ApiProperty({
     description: 'id del item',
     example: 2,
   })
-  @Expose()
   itemId: number
 
   @ApiProperty({
     description: 'id del material del ítem',
     example: 1,
   })
-  @Expose()
   materialId: number
 
   @ApiProperty({
     description: 'el material es principal?',
     example: true,
   })
-  @Expose()
   isMainMaterial: boolean
 
-  @Exclude()
-  registrationDate: Date
-
-  @Exclude()
-  updateDate: Date
+  @ApiProperty({
+    description: 'Material del ítem',
+  })
+  material: MaterialResDto
 }
